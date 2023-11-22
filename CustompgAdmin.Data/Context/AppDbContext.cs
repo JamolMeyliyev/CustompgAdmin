3289074cbc91789d355cc43744906942fb065213
jamol <jamol@LAPTOP-5TNOU2IH>
2023-11-22 19:42:37 +0500

    AppDbContext

---
+++ CustompgAdmin.Data/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CustompgAdmin.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustompgAdmin.Data.Context;
+
+public class AppDbContext:DbContext
+{
+   public AppDbContext(ConnectionDB db)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(CreateConnectString());
+
+
+    }
+    public string CreateConnectString(ConnectionDB connectionDB)
+    {
+        return  connectionDB.Host + ";" + connectionDB.DatabaseName + ";" + connectionDB.Username + ";" + connectionDB.Password;
+    }
+    
+}
