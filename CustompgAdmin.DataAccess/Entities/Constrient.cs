5258b4ad8447b57d05ce2e1e460b69dd20825f68
jamol <jamol@LAPTOP-5TNOU2IH>
2023-11-23 22:08:24 +0500

    a

---
+++ CustompgAdmin.DataAccess/Entities/Constrient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustompgAdmin.DataAccess.Entities;
+
+public class Constrient
+{
+}
