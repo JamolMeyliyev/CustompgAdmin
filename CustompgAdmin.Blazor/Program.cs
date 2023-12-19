950c9c942c533946a9e3cf65f727c70d873f6808
Jamol Meyliyev <113878950+JamolMeyliyev@users.noreply.github.com>
2023-12-19 11:50:33 +0500

    Local

--- CustompgAdmin.Blazor/Program.cs
+++ CustompgAdmin.Blazor/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7141") });
 
 await builder.Build().RunAsync();
