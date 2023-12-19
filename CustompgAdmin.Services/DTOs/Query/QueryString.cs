54cfa61ebfacd8cef84e72edb11f225513f099c0
Jamol Meyliyev <113878950+JamolMeyliyev@users.noreply.github.com>
2023-12-19 14:19:44 +0500

    new

---
+++ CustompgAdmin.Services/DTOs/Query/QueryString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustompgAdmin.Services.DTOs.Query;
+
+public class QueryForString
+{
+    public int Id { get; set; }
+    public string Query { get; set; }
+}
