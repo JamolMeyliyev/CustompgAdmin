4a4ec045c53f32e0d885ac7b55967a42ff3039cf
Jamol Meyliyev <113878950+JamolMeyliyev@users.noreply.github.com>
2023-12-20 22:45:23 +0500

    a

---
+++ CustompgAdmin.Blazor/Models/CreateModel/CreateColumnModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CustompgAdmin.Blazor.Models.CreateModel;
+
+public class CreateColumnModel
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("dataType")]
+    public int DataType { get; set; }
+    [JsonPropertyName("is_Nullable")]
+    public bool? Is_Nullable { get; set; }
+    [JsonPropertyName("isPrimaryKey")]
+    public bool? IsPrimaryKey { get; set; }
+    [JsonPropertyName("isUnique")]
+    public bool? IsUnique { get; set; }
+    [JsonPropertyName("maxLength")]
+    public int? MaxLength { get; set; }
+    [JsonPropertyName("defaultData")]
+    public object? DefaultData { get; set; }
+
+}
+
+
---
+++ CustompgAdmin.Blazor/Models/CreateModel/CreateTableModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CustompgAdmin.Blazor.Models.CreateModel;
+
+public class CreateTableModel
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("createColumns")]
+    public List<CreateColumnModel>? Columns { get; set; }
+}
